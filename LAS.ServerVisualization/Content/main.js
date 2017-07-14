248a23cb1eb0991b7ee00212b2072fea54e4b780
Antoine Cailliau <antoine.cailliau@uclouvain.be>
2017-07-14 11:26:20 +0200

    Updates API key

--- LAS.ServerVisualization/Content/main.js
+++ LAS.ServerVisualization/Content/main.js
@@ -1,13 +1,19 @@
 ﻿
+//var mymap = L.map('mapid').setView([50.8234575,4.3831473], 14);
 var mymap = L.map('mapid').setView([50.8234575,4.3831473], 14);
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
-	maxZoom: 18,
-	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-		'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-		'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-	id: 'mapbox.streets'
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.light',
+    accessToken: 'pk.eyJ1IjoiYW5jYWlsbGlhdSIsImEiOiJjajUzbW90NXQwNnF6MzJtd3RrdzB6OWppIn0.SR-d22ltbfWMD_LxkwR4HQ'
 }).addTo(mymap);
+
+// L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}' + accessToken, {
+// 	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+//     maxZoom: 18,
+//     id: 'mapbox.streets',
+//     accessToken: 'pk.eyJ1IjoiYW5jYWlsbGlhdSIsImEiOiJjajUzbW90NXQwNnF6MzJtd3RrdzB6OWppIn0.SR-d22ltbfWMD_LxkwR4HQ'
+// }).addTo(mymap);
 
 var ambulances = {};
 var incidents = {};
