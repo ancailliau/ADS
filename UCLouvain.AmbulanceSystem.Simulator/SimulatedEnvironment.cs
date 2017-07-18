ccc15b783a9bb029a9ccb58ad3f3d1ed45235216
Antoine Cailliau <antoine.cailliau@uclouvain.be>
2017-07-18 08:49:05 +0200

    Fixes for monitoring

--- UCLouvain.AmbulanceSystem.Simulator/SimulatedEnvironment.cs
+++ UCLouvain.AmbulanceSystem.Simulator/SimulatedEnvironment.cs
@@ -9,12 +9,30 @@ namespace UCLouvain.AmbulanceSystem.Simulator
 		static Random r = new Random();
 		static DateTime startTime = DateTime.Now;
 
+        static TimeSpan start_traffic_jam = new TimeSpan(7, 0, 0);
+        static TimeSpan end_traffic_jam = new TimeSpan(9, 0, 0);
+
+        static TimeSpan start_forget_button = new TimeSpan(3, 0, 0);
+        static TimeSpan end_forget_button = new TimeSpan(6, 0, 0);
+
+        static TimeSpan start_avls_failure = new TimeSpan(14, 0, 0);
+        static TimeSpan end_avls_failure = new TimeSpan(16, 0, 0);
+        
 		public static bool SendPosition(SimulatedAmbulance a)
 		{
 			var coin = r.NextDouble();
-			if (coin > .65) {
-				return false;
-			}
+
+            // Scenario 3
+            var now = DateTime.Now.TimeOfDay;
+            if (now > start_avls_failure & now < end_avls_failure) {
+                if (coin > .6) {
+                    return false;
+                }
+            } else {
+                if (coin > .2) {
+                    return false;
+                }
+            }
 
 			return true;
 		}
@@ -32,8 +50,10 @@ namespace UCLouvain.AmbulanceSystem.Simulator
 		public static bool PressOnSceneButton(SimulatedAmbulance a)
 		{
 			var coin = r.NextDouble();
-			// Scenario 2
-			if (DateTime.Now - startTime > TimeSpan.FromSeconds(15 * 60)) {
+
+            // Scenario 2
+            var now = DateTime.Now.TimeOfDay;
+            if (now > start_forget_button & now < end_forget_button) {
 				if (coin > .7) {
 					return false;
 				}
@@ -50,7 +70,8 @@ namespace UCLouvain.AmbulanceSystem.Simulator
 		{
 			var coin = r.NextDouble();
 			// Scenario 2
-			if (DateTime.Now - startTime > TimeSpan.FromSeconds(15 * 60)) {
+            var now = DateTime.Now.TimeOfDay;
+            if (now > start_forget_button & now < end_forget_button) {
 				if (coin > .7) {
 					return false;
 				}
@@ -68,7 +89,8 @@ namespace UCLouvain.AmbulanceSystem.Simulator
 			var coin = r.NextDouble();
 
 			// Scenario 2
-			if (DateTime.Now - startTime > TimeSpan.FromSeconds(15 * 60)) {
+            var now = DateTime.Now.TimeOfDay;
+            if (now > start_forget_button & now < end_forget_button) {
 				if (coin > .2) {
 					return false;
 				}
@@ -85,15 +107,16 @@ namespace UCLouvain.AmbulanceSystem.Simulator
 			var coin = r.NextDouble();
 
 			// Scenario 1
-			//if (DateTime.Now - startTime > TimeSpan.FromSeconds(15 * 60)) {
-			//	if (coin > .7) {
-			//		return false;
-			//	}
-			//} else {
+            var now = DateTime.Now.TimeOfDay;
+            if (now > start_traffic_jam & now < end_traffic_jam) {
+				if (coin > .7) {
+					return false;
+				}
+			} else {
 				if (coin > .01) {
 					return false;
 				}
-			//}
+			}
 
 			return true;
 		}
